0cc4da13bc67ecba13e854f3caf3509ad84ded02
Munir Uddin <munirchittagong@yahoo.com>
2019-10-11 21:04:54 +0100

    Observer design pattern in ruby

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'pry'
+gem 'rspec'
+gem 'guard'
+gem 'guard-rspec'
+gem 'terminal-notifier-guard'
---
+++ spec/template_method_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require_relative "../lib/template_method"
+
+describe "Template Method Pattern" do
+  let(:hero) { Hero.new }
+
+
+end
